5e7f50577e3bc773cce0af7f0db2a3872a9cb96c
Danny Olsen <dannyolsen1980@gmailcom>
2025-03-07 08:48:19 +0100

    Update .gitignore tracking

--- shroombox3/run_web.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /home/danny/Github/shroombox/shroombox3/env_shrooms/bin/activate
-python3 /home/danny/Github/shroombox/shroombox3/web/app.py
--- shroombox3/venv/lib/python3.11/site-packages/tapo/responses/child_device_list_power_strip_result/__init__.pyi
+++
@@ -1,1 +0,0 @@
-from .power_strip_plug_result import *
